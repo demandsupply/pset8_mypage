11a5e4f425554bc72233a8ca63f8a65bf14fb6b7
demandsupply <giova.zol92@gmail.com>
2023-12-31 14:17:26 +0100

    added a js function where when a user clicks the button content will be displayed/hided, and the button icon will rotate. Fixed some styling

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// Create a function that will show/hide content when the button is clicked
+// Declare variables
 
 let button = document.getElementsByClassName("button-show-p");
 let bttnId = document.getElementById("button1");
+let bttn2 = document.querySelector('[data-button2]');
 let p = document.getElementById("hideContent");
 let childrenLenght = document.querySelector(".bio-container").children.length;
 let toggleP = document.querySelectorAll(".toggle-function");
+let svg = document.querySelectorAll("[data-svg]");
 
-
+// Function 1
 // bttnId.addEventListener('click', function() {
 //         console.log("clicked! number of childrens is" + children);
 //         if (p.style.display === 'none' || p.style.display === "") {
@@ -14,18 +16,32 @@ let toggleP = document.querySelectorAll(".toggle-function");
 //         } else {
 //             p.style.display = 'none';
 //         }
-
 //         });
 
+// Function 2
 // Create a function where when the user clicks a button the content of that button will be shown, and the content of other buttons will be hide 
+// for (let i = 0; i < childrenLenght; i++) {
+//     button[i].addEventListener('click', function() {
+//         console.log('clicked button ' + (i + 1));
+//         for (let j = 0; j < childrenLenght; j++) {
+//             if (j != i) {
+//                 toggleP[j].classList.add("p-hide");
+//             }
+//         }
+//         toggleP[i].classList.remove("p-hide");
+//     });
+// }
+
+// Function 3
+// Create a function where when the user clicks a button the content will be displayed and vice versa
 for (let i = 0; i < childrenLenght; i++) {
     button[i].addEventListener('click', function() {
-        console.log('clicked button ' + (i + 1));
-        for (let j = 0; j < childrenLenght; j++) {
-            if (j != i) {
-                toggleP[j].classList.add("p-hide");
-            }
-        }
-        toggleP[i].classList.remove("p-hide");
+        console.log('button ' + (i + 1) + ' clicked!');
+        toggleP[i].classList.toggle("p-hide");
+        svg[i].classList.toggle("svg-rotate");
     });
 }
+
+bttn2.addEventListener('click', function() {
+    console.log('helloooo');
+});
